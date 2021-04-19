4336c4ff23776fbf4fbc00d05d5cd6859e5584c5
Matheus Schneider <matheus.ssm@gmail.com>
2021-04-19 17:49:23 -0300

    added GitHubAuth using nextauth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -2,18 +2,19 @@ import styles from './styles.module.scss';
 
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
-    <button className={styles.signInButton}>
+  return session ? (
+    <button className={styles.signInButton} onClick={() => signOut()}>
       <FaGithub color='#04d361' />
-      Matheus Schneider
+      {session.user.name}
       <FiX color='#737380' className={styles.closeIcon} />
     </button>
   ) : (
-    <button className={styles.signInButton}>
+    <button className={styles.signInButton} onClick={() => signIn('github')}>
       <FaGithub color='#eba417' />
       Sign in with GitHub
     </button>
